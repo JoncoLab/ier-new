0d5cd46f4adf2f460016da97d91dfa2bc31bd901
Sher <max.pechko@gmail.com>
2017-10-03 22:20:01 +0300

    0.0.3

---
+++ src/comp/Header/NavMenu.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class NavMenu extends Component {
+    render() {
+        return {
+            <nav>
+                
+            </nav>
+        };
+    }
+}
