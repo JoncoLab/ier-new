2d6220b57a9db2af4f4cd4679430ae87c0f11902
Sher <max.pechko@gmail.com>
2017-10-10 07:52:58 +0300

    0.1.6

--- src/comp/Footer/Contacts/ContactsBlock.js
+++ src/comp/Footer/Contacts/ContactsBlock.js
@@ -1,18 +1,79 @@
 import React, { Component } from 'react';
+import * as $ from "jquery";
 
 export class ContactsBlock extends Component {
+    handleClick() {
+        const contactsBlock = $('.contacts-block');
+
+        contactsBlock.removeClass('active');
+    }
     render() {
         return (
-            <table>
-                <tr>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </table>
+            <section className="contacts-block">
+                <table>
+                    <caption>Деканат факультету міжнародних відносин:
+                        <button id="contacts-close-btn" onClick={this.handleClick}>X</button>
+                    </caption>
+                    <tr>
+                        <th>Телефон:</th>
+                        <td>+38 (03122) 3-33-41</td>
+                    </tr>
+                    <tr>
+                        <th>E-mail:</th>
+                        <td>official@uzhnu.edu.ua</td>
+                    </tr>
+                    <tr>
+                        <th>Адреса:</th>
+                        <td>м.Ужгород, вул.Університетська 14, фуд.510</td>
+                    </tr>
+                    <tr>
+                        <th>Поштовий індекc:</th>
+                        <td>88000</td>
+                    </tr>
+                </table>
+                <table>
+                    <caption>Ректорат УжНУ:</caption>
+                    <tr>
+                        <th>Телефон:</th>
+                        <td>+38 (03122) 3-33-41</td>
+                    </tr>
+                    <tr>
+                        <th>E-mail:</th>
+                        <td>official@uzhnu.edu.ua</td>
+                    </tr>
+                    <tr>
+                        <th>Адреса:</th>
+                        <td>Україна, Закарпатська обл., м.Ужгород, пл.Народна 3</td>
+                    </tr>
+                    <tr>
+                        <th>Факс:</th>
+                        <td>+38 (03122) 3-42-02</td>
+                    </tr>
+                    <tr>
+                        <th>Поштовий індекс:</th>
+                        <td>88000</td>
+                    </tr>
+                </table>
+                <table>
+                    <caption>Приймальна комісія УжНУ:</caption>
+                    <tr>
+                        <th>Телефон:</th>
+                        <td>+38 (03122) 64-30-84, 066-571-62-40, 096-123-89-67</td>
+                    </tr>
+                    <tr>
+                        <th>E-mail:</th>
+                        <td>admission@uzhnu.edu.ua</td>
+                    </tr>
+                    <tr>
+                        <th>Адреса:</th>
+                        <td>Україна, Закрпатська обл., м.Ужгород, вул.Університетська 14, кімн.228</td>
+                    </tr>
+                    <tr>
+                        <th>Поштовий індекс:</th>
+                        <td>88000</td>
+                    </tr>
+                </table>
+            </section>
         );
     }
 }
--- src/comp/Footer/Download/DownloadBlock.js
+++ src/comp/Footer/Download/DownloadBlock.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import * as $ from "jquery";
 
 export class DownloadBlock extends Component {
+    handleClick() {
+        const downloadBlock = $('.DownloadBlock');
+
+        downloadBlock.removeClass('active');
+    }
+
     render() {
         return (
-            <div className="DownloadBlock">
-                <a href="#">Завантажити для IOS</a>
-                <a href="#">завантажити для Android</a>
+            <div className="DownloadBlock" onClick={this.handleClick}>
+                <a href="#">Завантажити для IOS ♪</a>
+                <a href="#">Завантажити для Android ♫</a>
             </div>
         );
     }
