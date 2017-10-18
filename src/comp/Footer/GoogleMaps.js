30f3d16bf1acb0c68e6e9ac795b28e8fc39c920d
Saladin <nem97.sv@gmail.com>
2017-10-18 04:26:12 +0300

    0.2.6

--- src/comp/Footer/GoogleMaps.js
+++ src/comp/Footer/GoogleMaps.js
@@ -5,6 +5,7 @@ export class GoogleMaps extends Component {
         return  (
             <iframe
                 className="GoogleMaps"
+                frameBorder="0"
                 src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ9QaRKjQYOUcRR8L7SNcV7aI&key=AIzaSyCtwEcRTT0pjjU_ux-F4SvN0vgZvGuCn00"
             >
             </iframe>
--- src/comp/Main/Gallery.js
+++ src/comp/Main/Gallery.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import * as $ from "jquery";
 
 export class Gallery extends Component {
     constructor(props) {
@@ -8,19 +9,37 @@ export class Gallery extends Component {
         };
 
         this.switchItem = this.switchItem.bind(this);
+        this.pointSwitcher = this.pointSwitcher.bind(this);
         this.next = this.next.bind(this);
         this.previous = this.previous.bind(this);
     }
+    captions = [
+        "День перекладача",
+        "Ще якась новина, я забув, сорі",
+        "Міс Факультету Міжнародних Економічних Відносин 2017",
+        "Ще якась новина, яку я теж забув",
+        "Та новина, де фотка з грошима"
+    ];
     switchItem(item) {
+        let target = item >= 0 ?
+            (item <= 4 ?
+                item : 4) :
+            0;
         this.setState({
-            currentItem: item
+            currentItem: target
         });
+        const counters = $('.gallery .caption .counter .counter-point');
+        counters.removeClass("active");
+        $(counters[target]).addClass("active");
     }
     next() {
         this.switchItem(this.state.currentItem + 1);
     }
     previous() {
         this.switchItem(this.state.currentItem - 1);
+    }
+    pointSwitcher(item) {
+        this.switchItem(item);
     }
     render() {
         return (
@@ -29,21 +48,24 @@ export class Gallery extends Component {
                     ♠
                 </span>
                 <div className="container" style={{transform: "translateX(" + this.state.currentItem * -100 + "%)"}}>
+                    <img alt="img" src="./img/img-1.jpeg"/>
+                    <img alt="img" src="./img/img-1.jpeg"/>
                     <img alt="igm" src="./img/hot-new-miss.jpg"/>
                     <img alt="img" src="./img/img-1.jpeg"/>
-                    <img alt="img" src="./img/img-2.jpg"/>
+                    <img alt="img" src="./img/img-1.jpeg"/>
                 </div>
                 <div className="caption">
                     <p>
-                        {/*{this.state.currentItem + " is current item"}*/}
-                        Міс Факультету Міжнародних Економічних Відносин 2017
+                        {
+                            this.captions[this.state.currentItem]
+                        }
                     </p>
                     <div className="counter">
-                        <span className="counter-point"> </span>
-                        <span className="counter-point active"> </span>
-                        <span className="counter-point"> </span>
-                        <span className="counter-point"> </span>
-                        <span className="counter-point"> </span>
+                        <span onClick={(e) => this.switchItem(0)} className="counter-point active"> </span>
+                        <span onClick={(e) => this.switchItem(1)} className="counter-point"> </span>
+                        <span onClick={(e) => this.switchItem(2)} className="counter-point"> </span>
+                        <span onClick={(e) => this.switchItem(3)} className="counter-point"> </span>
+                        <span onClick={(e) => this.switchItem(4)} className="counter-point"> </span>
                     </div>
                 </div>
                 <span className="arrow right" onClick={this.next}>
